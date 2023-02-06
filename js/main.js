15f598c296b0da371c046b75fbf9e2ccc96a6d66
felipebaguena <felipebaguena@gmail.com>
2023-02-06 17:57:26 +0100

    ejercicios de pseudocódigo

---
+++ js/main.js
@@ -0,0 +1,73 @@
+// RETOS PSEUDOCÓDIGO
+
+// EJERCICIO 1
+
+// let num1 = parseInt(prompt("Introduce el primer número"));
+// let num2 = parseInt(prompt("Introduce el segundo número"));
+// let resultado = num1 + num2;
+
+// console.log ('La suma de los dos números es ', resultado);
+
+// EJERCICIO 2
+
+// let num1 = parseInt(prompt('Introduce el primer número'));
+// let num2 = parseInt(prompt('Introduce el segundo número'));
+
+// if (num1 === num2){
+//     console.log ("Los números son iguales");
+// }else if (num1 > num2){
+//     console.log ("El primer número es el mayor");
+// }else{
+//     console.log ("El segundo número es el mayor");
+// }
+
+// EJERCICIO 3
+
+// let A = parseInt(prompt('Introduce el valor A'));
+// let B = parseInt(prompt("Introduce el valor B"));
+// let C = parseInt(prompt("Introduce el valor C"));
+
+// if (A === B && B === C){
+//     console.log ("Los tres números son iguales");
+// }else if (A > B && A > C){
+//     console.log (A, " es el mayor de los tres");
+// }else if (B > A && B > C){
+//     console.log (B, " es el mayor de los tres");
+// }else{
+//     console.log (C, " es el mayor de los tres");
+// }
+
+// EJERCICIO 4
+
+// let cateto1 = 5;
+// let cateto2 = 7;
+// let hipotenusaAlCuadrado = (cateto1 ** 2) + (cateto2 **2);
+// let hipotenusa = Math.sqrt(hipotenusaAlCuadrado);
+
+// console.log ("La hipotenusa es ", hipotenusa);
+
+// EJERCICIO 5
+
+
+// let suma = 0
+
+// for (let i = 0 ; i < 10; i++){
+//     let num = parseInt(prompt("Introduce el número que quieres sumar"));
+//     suma += num; 
+// }
+
+// console.log ("El total es ", suma);
+
+// EJERCICIO 6
+
+// let suma = 0
+// let sumasTotales = parseInt(prompt("¿Cuántos números quieres sumar?"));
+
+// for (let i = 0 ; i < sumasTotales; i++){
+//     let num = parseInt(prompt("Introduce el número que quieres sumar"));
+//     suma += num;
+// }
+
+// console.log ("El total es ", suma);
+
+
